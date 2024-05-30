2b8cac419ffdfe77ae35f6ac670d710df6ea134d
yuvraj Sinh <yuvrajsinh123428@gmail.com>
2024-05-30 09:36:51 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,226 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <div>
+  <div id="uni">
+    <h1 id="hwe">
+      Access 4,000+ courses, 100+ SkillSets, and 1,000+ Guided Projects from top
+      universities and companies
+    </h1>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2020/10/il_square_logo_large.png"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2020/10/yale_square_logo_large.jpeg"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2020/10/mu_square_logo_large.jpeg"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2019/01/ucd.png"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/GreyLogoName192x192.png"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2019/11/atlassian-logo-gradient-vertical-white.jpeg"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2019/01/uc-sandiego.png"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2019/01/nyu.png"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2020/03/University-of-Colorado-System.jpg"
+        alt=""
+      />
+    </div>
+  </div>
+  <div id="skill_box">
+    <h1>High-impact skill development for all teams</h1>
+    <div id="box_cont" className="boxcontskill">
+      <div className="box skillbox1">
+        <div className="box_image">
+          <img
+            alt="Build in-demand skills for the business"
+            src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2021/03/gettyimages-1192313070-170667a_Edit-1.jpg"
+          />
+        </div>
+        <div className="box_info">
+          <div className="box_det">
+            <h3>Build in-demand skills for the business</h3>
+            <p>
+              Accelerate skill acquisition with SkillSets, turnkey job-based
+              learning programs.
+            </p>
+          </div>
+          <div className="box_link">
+            <a
+              href="https://www.coursera.org/business/products?utm_campaign=c4b&utm_content=in-demand-skills-hp&utm_medium=website&utm_source=enterprise#product-skills"
+              target="_blank"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="box skillbox1">
+        <div className="box_image">
+          <img
+            alt="Build in-demand skills for the business"
+            src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2021/03/gettyimages-1060903200-170667a-1.jpg"
+          />
+        </div>
+        <div className="box_info">
+          <div className="box_det">
+            <h3>Fast-track digital transformation with Academies</h3>
+            <p>
+              Deploy targeted learning for tech teams and build skills for
+              everyone.
+            </p>
+          </div>
+          <div className="box_link">
+            <a
+              href="https://www.coursera.org/business/products?utm_campaign=c4b&utm_content=in-demand-skills-hp&utm_medium=website&utm_source=enterprise#product-skills"
+              target="_blank"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="box skillbox1">
+        <div className="box_image">
+          <img
+            alt="Build in-demand skills for the business"
+            src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2021/03/gettyimages-1175077096-170667a_Edit-1.jpg"
+          />
+        </div>
+        <div className="box_info">
+          <div className="box_det">
+            <h3>Drive skill mastery with hands-on learning</h3>
+            <p>
+              Deliver applied tech and data skills with 1,000+ Guided Projects
+              and in-course lab assignments.
+            </p>
+          </div>
+          <div className="box_link">
+            <a
+              href="https://www.coursera.org/business/products?utm_campaign=c4b&utm_content=in-demand-skills-hp&utm_medium=website&utm_source=enterprise#product-skills"
+              target="_blank"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="box skillbox1">
+        <div className="box_image">
+          <img
+            alt="Build in-demand skills for the business"
+            src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2021/03/gettyimages-1205248124-170667a_Edit-1-1.jpg"
+          />
+        </div>
+        <div className="box_info">
+          <div className="box_det">
+            <h3>Track, measure, and benchmark actual skill proficiency</h3>
+            <p>
+              Quantify employee progress in technical and human skills with
+              continuous assessments.
+            </p>
+          </div>
+          <div className="box_link">
+            <a
+              href="https://www.coursera.org/business/products?utm_campaign=c4b&utm_content=in-demand-skills-hp&utm_medium=website&utm_source=enterprise#product-skills"
+              target="_blank"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="comp">
+    <h1 id="compHead">Companies that innovate use Coursera</h1>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2019/04/pg.png"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/Home_CustomerLogo_Loreal-1.png"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/11/SignifyLogo-1.png"
+        alt=""
+      />
+    </div>
+    <div id="horr">
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2021/07/GE-Logo-1-e1626191342716.png"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2019/04/danone.png"
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/02/Home_CustomerLogo_Tata-3.png"
+        alt=""
+      />
+    </div>
+    <div className="clear" />
+  </div>
+  <div id="yt">
+    <img
+      src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2021/03/c4b_1.jpg"
+      alt=""
+    />
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default App
